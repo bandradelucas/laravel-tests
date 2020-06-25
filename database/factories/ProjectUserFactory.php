392bdf4f720a6d37b91510eecf9fb4f9bcc8fdca
Lucas de Andrade <bandradelucas@gmail.com>
2020-06-24 22:06:49 -0300

    :card_file_box: Some basic models

---
+++ database/factories/ProjectUserFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ProjectUser;
+use Faker\Generator as Faker;
+
+$factory->define(ProjectUser::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/ProjectUserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
